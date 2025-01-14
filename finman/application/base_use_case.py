0aac44849459415b9e002f3aa34602a72ab1302a
EgorD <egor.d912@gmail.com>
2025-01-14 22:15:54 +0200

    fix isort, add use case base class

---
+++ finman/application/base_use_case.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+
+InputType = TypeVar("InputType")
+OutputType = TypeVar("OutputType")
+
+
+class UseCase(ABC, Generic[InputType, OutputType]):
+    @abstractmethod
+    def __call__(self, input_data: InputType) -> OutputType:
+        raise NotImplementedError
